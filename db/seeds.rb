fed66690a8d75ad49138a7b524133b86dd156749
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-03 20:27:22 +0630

    fix rubocop errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@ second_post = Post.create(author_id: first_user, title: 'Hello World', text: 'Th
 third_post = Post.create(author_id: second_user, title: 'Hi', text: 'This is my third post')
 fourth_post = Post.create(author_id: second_user, title: 'Hi World', text: 'This is my fourth post')
 
-Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
-Comment.create(post_id: first_post, author_id: first_user, text: 'Hi Lilly!' )
-Comment.create(post_id: first_post, author_id: second_user, text: 'Hi , this is my first comment' )
-Comment.create(post_id: second_post, author_id: first_user, text: 'Hi , this is my second comment' )
-Comment.create(post_id: second_post, author_id: second_user, text: 'Hi , this is my third comment' )
-Comment.create(post_id: second_post, author_id: first_user, text: 'Hi , this is my fourth comment' )
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!')
+Comment.create(post_id: second_post, author_id: first_user, text: 'Hi Lilly!')
+Comment.create(post_id: third_post, author_id: second_user, text: 'Hi , this is my first comment')
+Comment.create(post_id: fourth_post, author_id: first_user, text: 'Hi , this is my second comment')
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi , this is my third comment')
+Comment.create(post_id: second_post, author_id: first_user, text: 'Hi , this is my fourth comment')
 
 Like.create(post_id: first_post, author_id: second_user)
 Like.create(post_id: second_post, author_id: first_user)

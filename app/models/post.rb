e2933b91a789acebe9df6a1cce0dd7cc4656da0a
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-04 16:00:37 +0630

    add tests for user model methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :update_posts_counter
 
-  validation :title, presence: true, length: { maximum: 250 }
-  validation :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validation :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)

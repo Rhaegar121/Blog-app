6d627f444fd780bf260beb1593d0ed33cbfb3945
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-08 18:33:16 +0630

    fix rubocop error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root 'users#index'
   get '/users/:id(.:format)', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'

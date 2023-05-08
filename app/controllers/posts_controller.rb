756a9610504342a7d64a72f585c21dde9f5f5c2b
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-08 18:45:22 +0630

    remove commented code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,2 @@
 class PostsController < ApplicationController
-  # def index
-  #   @user = User.find_by_id(params[:user_id])
-  #   @posts = @user.posts
-  # end
-
-  # def show
-  #   @post = Post.find_by_id(params[:id])
-  # end
 end

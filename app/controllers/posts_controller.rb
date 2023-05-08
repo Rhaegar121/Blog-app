acd482450ce323edcc0a04aa428d28f776dfdeea
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-08 17:42:44 +0630

    fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.find_by_id(params[:user_id])
+    @posts = @user.posts
+  end
 
-    def index
-        @user = User.find_by_id(params[:user_id])
-        @posts = @user.posts
-    end
-
-    def show
-        @post = Post.find_by_id(params[:id])
-    end
+  def show
+    @post = Post.find_by_id(params[:id])
+  end
 end

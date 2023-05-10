127e8f9b3e5c78598df2b7f7b8bb59152f439bb8
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-10 19:57:38 +0630

    add tests for user controller in users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,39 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) do
+      get '/'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Here is a list of Users')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      get '/users/1'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Here is a list of Users for a given id')
+    end
   end
 end

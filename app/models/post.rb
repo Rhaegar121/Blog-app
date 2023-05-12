e1058ae6f2c988cc6d3d137594f42be6ff236254
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-12 19:05:47 +0630

    fix rubocop erros

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end

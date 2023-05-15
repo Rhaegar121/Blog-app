2f23d1fee699083169d3b8c630dee27fd1fa4275
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-15 21:06:15 +0630

    add tests for post_show_spec.rb file

---
+++ spec/features/post_show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Post show', type: :feature do
+    before :each do
+        @user = User.create(name: 'Ross', photo: 'https://picsum.photos/id/23/200', bio: 'Teacher from Mexico.', posts_counter: 0)
+        @post = Post.create(author_id: @user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+        @comment1 = Comment.create(post_id: @post.id, author_id: @user.id, text: 'This is my first comment')
+        @comment2 = Comment.create(post_id: @post.id, author_id: @user.id, text: 'This is my second comment')
+        visit user_post_path(user_id: @post.author_id, id: @post.id)
+    end
+
+    it 'can see post title' do
+        expect(page).to have_content 'Hello'
+    end
+
+    it 'can see who wrote the post' do
+        expect(page).to have_content 'Ross'
+    end
+
+    it 'can see how many comments it has' do
+        expect(page).to have_content(@post.comments_counter)
+    end
+
+    it 'can see how many likes it has' do
+        expect(page).to have_content(@post.likes_counter)
+    end
+
+    it 'can see post body' do
+        expect(page).to have_content 'This is my first post'
+    end
+end

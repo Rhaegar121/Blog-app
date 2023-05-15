80a9f95baf7657cc1ded4eefc4a6d959cfcc1565
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-15 21:23:54 +0630

    fix rubocop errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
-class Like < ApplicationRecord
+Layout/EndOfLine: Carriage return character detected.class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post'
 

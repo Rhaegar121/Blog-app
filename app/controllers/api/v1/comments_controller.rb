89feca03f1a19e3c1bd45276c9d56bc1d971465f
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-18 17:05:22 +0400

    Add index api

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::CommentsController < ApplicationController
     load_and_authorize_resource
 
-    # def index
-    #   @comments = Comment.where(post_id: params[:post_id]).order('created_at')
-    #   render json: { success: true, data: { comments: @comments } }
-    # end
+    def index
+      @comments = Comment.where(post_id: params[:post_id]).order('created_at')
+      render json: { success: true, data: { comments: @comments } }
+    end
 
     # def create
     #     @post = Post.find(params[:post_id])

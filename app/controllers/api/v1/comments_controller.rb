fc40764428537e2cdd98aa3ceab1299bde18f12c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-18 18:22:58 +0630

    add comments controller in controllers/api/v1 dir

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::CommentsController < ApplicationController
+    load_and_authorize_resource
+
+    def index
+      @user = User.find(params[:user_id])
+      @comments = @user.posts.joins(:comments).select('comments.*')
+      render json: { success: true, data: { comments: @comments } }
+    end
+
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = Comment.new(comment_params)
+        @comment.author_id = current_user.id
+        @comment.post_id = @post.id
+        
+        if @comment.save
+          render json: { success: true, data: { comment: @comment } }, status: :created
+        else
+          render json: { success: false, error: @comment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+      
+    private
+      
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+end
+  
